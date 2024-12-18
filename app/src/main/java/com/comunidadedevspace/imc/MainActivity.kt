254450f5c5815dad01432430f577da91756aa2d4
beep saude <beepsaude@MacBook-Pro-de-Rachell.local>
2024-12-17 23:24:17 -0300

    criacao layout e calculo imc

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,29 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputPeso = findViewById<TextInputEditText>(R.id.edtpeso)
+        val inputAltura = findViewById<TextInputEditText>(R.id.edtaltura)
+        val btnCalcular = findViewById<Button>(R.id.btncalcular)
+
+        btnCalcular.setOnClickListener {
+
+            val peso: Float = inputPeso.text.toString().toFloat()
+            val altura: Float = inputAltura.text.toString().toFloat()
+            val altura2 = altura * altura
+
+            val imc = peso / altura2
+
+            println("Teste IMC: " + imc)
+        }
+
     }
 }
