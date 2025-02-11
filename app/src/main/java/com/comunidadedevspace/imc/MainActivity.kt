efaf25a3efbeb3e5a45a86d945f9ebb9dbd209c0
kotlinpj <kotlinpj.23@gmail.com>
2025-02-10 23:27:38 -0300

    criacao de activity result e exibicao de resultado IMC

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
@@ -24,16 +24,25 @@ class MainActivity : AppCompatActivity() {
             if (pesoStr == "" || alturaStr == "") {
                 Snackbar.make(
                     inputPeso,
-                    "Preencher os campos",
+                    "Preencher todos os campos",
                     Snackbar.LENGTH_SHORT
                 )
                     .show()
+
             } else {
                 val peso = pesoStr.toFloat()
                 val altura = alturaStr.toFloat()
 
                 val altura2 = altura * altura
                 val imc = peso / altura2
+
+
+                // Aqui  chama a tela de resultado
+                // Intent é a intencao para onde eu quero que a tela vá
+                // neste caso para tela de resultado (ResultActivity)
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, imc)
+                startActivity(intent)
 
                 println("Teste IMC: " + imc)
             }
